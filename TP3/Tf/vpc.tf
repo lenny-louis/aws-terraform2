f5fd684af4170afd1c9f7ab6cd539c76db528065
lenny-louis <louislennyy@gmail.com>
2020-08-19 15:04:12 +0200

    upd

--- TP3/Tf/vpc.tf
+++ TP3/Tf/vpc.tf
@@ -1,14 +1,14 @@
 # Changer le nom vpc_example pour un déploiement précis
 # Adapter le réseau
 resource "aws_vpc" "vpc_example" {
-  cidr_block = "10.42.1.0/24" # RFC 1918, réseau privé
+  cidr_block = "192.168.77.0/24" # RFC 1918, réseau privé
 }
 
 # Changer le nom subnet_example pour un déploiement précis
 # Adapter le réseau (voire en ajouter)
 resource "aws_subnet" "subnet_example" {
   vpc_id                  = aws_vpc.vpc_example.id # cf. ressource ci-dessus
-  cidr_block              = "10.42.1.0/24"
+  cidr_block              = "192.168.77.0/24"
   map_public_ip_on_launch = true
 }
 
