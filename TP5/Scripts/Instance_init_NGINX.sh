78f9927c06c4056ef4927b68fd4ed71336ead0dd
lenny-louis <louislennyy@gmail.com>
2020-08-20 16:30:54 +0200

    upd

--- TP5/Scripts/Instance_init_NGINX.sh
+++ TP5/Scripts/Instance_init_NGINX.sh
@@ -1,7 +1,7 @@
 
 
 sudo apt-get install -y nginx,
-      sudo mv /home/roote/gits/aws-terraform2/TP5/Scripts/oink /etc/nginx/sites-available/oink
+      sudo mv /home/roote/gits/aws-terraform2/TP5/Scripts/nginx.conf /etc/nginx/sites-available/nginx.conf
       sudo ln -s /etc/nginx/sites-available/oink /etc/nginx/sites-enabled/oink
       sudo rm /etc/nginx/sites-enabled/default
       sudo systemctl reload nginx
@@ -15,3 +15,14 @@ fi
 
 
 curl http://your_server_ip > /tmp/cloud-init-nginx.log
+
+cd nginx/etc/ssl
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout private/nginx-selfsigned.key \
+    -out ./nginx-selfsigned.crt
+
+
+openssl dhparam -out dhparam.pem 2048
+
+
